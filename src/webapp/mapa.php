97b5571310afca4382cf58e33dcd6e528c20f3c7
DimitryGarces <thebroyt.290@gmail.com>
2023-11-07 10:59:16 -0600

    Trazando ruta de prueba

--- src/webapp/mapa.php
+++ src/webapp/mapa.php
@@ -9,12 +9,27 @@
   <link rel="stylesheet" type="text/css" href="css/styles.css">
   <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/leaflet.locatecontrol/dist/L.Control.Locate.min.js" charset="utf-8"></script>
+  <script src="https://unpkg.com/leaflet-routing-machine@3.2.12/dist/leaflet-routing-machine.js"></script>
+  <link rel="stylesheet" href="https://unpkg.com/leaflet-routing-machine@3.2.12/dist/leaflet-routing-machine.css" />
+
   <style>
     /* Estilo CSS para el botón */
     .ubicacion-button {
       position: absolute;
       /* Posición absoluta */
       top: 10px;
+      /* Ajusta la posición desde la parte superior */
+      right: 10px;
+      /* Ajusta la posición desde la derecha */
+      z-index: 1000;
+      /* Capa z para estar encima del mapa */
+    }
+
+    /* Estilo CSS para el botón */
+    .ruta-button {
+      position: absolute;
+      /* Posición absoluta */
+      top: 50px;
       /* Ajusta la posición desde la parte superior */
       right: 10px;
       /* Ajusta la posición desde la derecha */
@@ -54,6 +69,7 @@
     <button id="btn-rechazar">Rechazar</button>
   </div>
   <button class="ubicacion-button" id="obtener-ubicacion">Obtener Ubicación</button>
+  <button class="ruta-button" id="ruta-ubicacion">Obtener Ruta</button>
 </body>
 
 
