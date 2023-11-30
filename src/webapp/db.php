180ea8f11f8d1cdd9276227224c9c75fc6bf40a0
Jonathan Montalvo <jmp20245@gmail.com>
2023-11-29 20:42:35 -0600

    se unieron las ramas

--- src/webapp/db.php
+++ src/webapp/db.php
@@ -1,20 +1,9 @@
 <?php
 
-/*try {
-    $con = mysqli_init();
-    mysqli_ssl_set($con, NULL, NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-    mysqli_real_connect($con, "gasgarces.mysql.database.azure.com", "gasgarces", "bleach123!", "gasgarces", 3306, MYSQLI_CLIENT_SSL);
-    
-    if ($con->connect_error) {
-        throw new Exception("Error de conexión a la base de datos: " . $con->connect_error);
-    }
-} catch (Exception $e) {
-    // Manejar la excepción, por ejemplo, registrándola o mostrándola
-    die($e->getMessage());
-}*/
 try {
     $con = mysqli_init();
-    mysqli_real_connect($con, "localhost", "gasgarces", "1234", "gasgarces", 3806);
+    mysqli_ssl_set($con, NULL, NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+    mysqli_real_connect($con, "gasgarces1.mysql.database.azure.com", "garces", "Bleach123", "gasgarces", 3306, MYSQLI_CLIENT_SSL);
 
     if ($con->connect_error) {
         throw new Exception("Error de conexión a la base de datos: " . $con->connect_error);
@@ -23,4 +12,5 @@ try {
     // Manejar la excepción, por ejemplo, registrándola o mostrándola
     die($e->getMessage());
 }
+
 ?>
